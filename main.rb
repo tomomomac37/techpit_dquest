983d0a28adbcb56507b871c626c70cafd3936fe2
Tomohiro Hori <tomomomac37@gmail.com>
2021-12-11 21:52:38 +0900

    3章終了

--- main.rb
+++ main.rb
@@ -136,3 +136,15 @@ loop do
   monster.attack(brave)
   break if brave.hp <= 0
 end
+
+battle_result = brave.hp > 0
+
+if battle_result
+  exp = (monster.offense + monster.defense) * 2
+  gold = (monster.offense + monster.defense) * 3
+  puts "#{brave.name}はたたかいに勝った"
+  puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+else
+  puts "#{brave.name}はたたかいに負けた"
+  puts "目の前が真っ暗になった"
+end
